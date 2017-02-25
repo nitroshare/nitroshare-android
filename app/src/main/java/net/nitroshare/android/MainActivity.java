5be861e1c2c90cc2d70dc8e25f19adf8221eb6c1
Nathan Osman <nathan@quickmediasolutions.com>
2017-02-25 11:08:57 -0800

    Fix storage permission issue.

--- app/src/main/java/net/nitroshare/android/MainActivity.java
+++ app/src/main/java/net/nitroshare/android/MainActivity.java
@@ -1,7 +1,10 @@
 package net.nitroshare.android;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import net.nitroshare.android.discovery.DiscoveryService;
@@ -12,6 +15,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Ensure we have write access to external storage
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int permission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                String[] permissions = {
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                };
+                requestPermissions(permissions, 0);
+            }
+        }
+
+        // TODO: error handling!
 
         // Start the discovery service
         Intent startIntent = new Intent(this, DiscoveryService.class);
