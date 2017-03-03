8b679f7f1df8d972f5e2d5bc333fe522f7bcf141
Nathan Osman <nathan@quickmediasolutions.com>
2017-03-02 23:10:55 -0800

    Prepare TransferService for receiving transfers.

--- app/src/main/java/net/nitroshare/android/transfer/TransferService.java
+++ app/src/main/java/net/nitroshare/android/transfer/TransferService.java
@@ -1,90 +1,108 @@
 package net.nitroshare.android.transfer;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
-import net.nitroshare.android.R;
 import net.nitroshare.android.bundle.Bundle;
 import net.nitroshare.android.bundle.FileItem;
 import net.nitroshare.android.discovery.Device;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Stack;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Receive incoming transfers and initiate outgoing transfers
+ *
+ * This service listens for new connections and instantiates Transfer instances
+ * to process them. It will also initiate a transfer when the appropriate
+ * intent is supplied.
+ */
 public class TransferService extends Service {
 
     private static final String TAG = "TransferService";
 
-    public static final String ACTION_INITIATE_TRANSFER = "initiate_transfer";
+    public static final String ACTION_START_LISTENING = "start_listening";
+    public static final String ACTION_STOP_LISTENING = "stop_listening";
+
+    public static final String ACTION_START_TRANSFER = "start_transfer";
     public static final String EXTRA_DEVICE = "device";
     public static final String EXTRA_URLS = "urls";
     public static final String EXTRA_FILENAMES = "filenames";
 
-    private NotificationManager mNotificationManager;
+    public static final String ACTION_STOP_TRANSFER = "stop_transfer";
+    public static final String EXTRA_TRANSFER = "transfer";
 
-    private final AtomicInteger mNotificationId = new AtomicInteger(0);
-    private final AtomicInteger mNumActiveTransfers = new AtomicInteger(0);
+    private static int STATE_STOPPED = 0;
+    private static int STATE_STARTED = 1;
+    private static int STATE_STOP = 2;
+    private static int STATE_STOPPING = 3;
+
+    private ServerSocketChannel mServerSocketChannel;
+    private AtomicInteger mSocketState = new AtomicInteger(STATE_STOPPED);
 
     /**
-     * Create a notification to show status of an ongoing transfer
-     * @param device remote device used for the transfer
-     * @return newly created Notification.Builder
+     * Start listening for new incoming connections
      *
-     * The notification initially displays the "connecting..." message and
-     * should be shown with startForeground().
+     * If the socket is already listening for new connections, then this method
+     * does nothing. It will continue to listen until stopped.
      */
-    private Notification.Builder buildSendTransferNotification(Device device) {
-        return new Notification.Builder(this)
-                .setCategory(Notification.CATEGORY_SERVICE)
-                .setContentTitle(getText(R.string.transfer_title))
-                .setContentText(getText(R.string.transfer_status_connecting))
-                .setSmallIcon(R.drawable.ic_stat_transfer)
-                .setProgress(0, 0, true);
+    private void startListening() {
+        if (mSocketState.compareAndSet(STATE_STOPPED, STATE_STARTED)) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        mServerSocketChannel = ServerSocketChannel.open();
+                        mServerSocketChannel.socket().bind(new InetSocketAddress(40818));
+
+                        Log.i(TAG, String.format("server bound to %d",
+                                mServerSocketChannel.socket().getLocalPort()));
+
+                        // Create the selector that will
+                        Selector selector = Selector.open();
+                        SelectionKey selectionKey = mServerSocketChannel.register(
+                                selector, SelectionKey.OP_ACCEPT);
+
+                        // Create new transfers for new connections
+                        while (true) {
+                            selector.select();
+                            if (mSocketState.compareAndSet(STATE_STOP, STATE_STOPPING)) {
+                                Log.i(TAG, "server shutting down");
+                                break;
+                            }
+                            if (selectionKey.isAcceptable()) {
+                                SocketChannel socketChannel = mServerSocketChannel.accept();
+                                new TransferWrapper(TransferService.this, new Transfer(
+                                        socketChannel, "Unknown")).run();
+                            }
+                        }
+
+                        // Shut down the server and indicate this is complete
+                        mServerSocketChannel.close();
+                        mSocketState.set(STATE_STOPPED);
+
+                    } catch (IOException e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                }
+            }).start();
+        }
     }
 
     /**
-     * Create a notification indicating a transfer has succeeded
-     * @param device remote device used for the transfer
+     * Shut down the server if it is running
      */
-    private void showSuccessNotification(Device device) {
-        String contentText = String.format(
-                getString(R.string.transfer_status_success),
-                device.getName()
-        );
-        mNotificationManager.notify(mNotificationId.incrementAndGet(), new Notification.Builder(this)
-                .setCategory(Notification.CATEGORY_STATUS)
-                .setContentTitle(getText(R.string.transfer_title))
-                .setContentText(contentText)
-                .setSmallIcon(R.drawable.ic_stat_transfer)
-                .build());
-    }
-
-    // TODO: error notification should include an action to retry the transfer
-    //   this is simpler than it sounds since we can just resend the original
-    //   intent that started the transfer
-
-    /**
-     * Create a notification indicating that a transfer failed
-     * @param device remote device used for the transfer
-     * @param errorMessage description of the error that occurred
-     */
-    private void showErrorNotification(Device device, String errorMessage) {
-        String contentText = String.format(
-                getString(R.string.transfer_status_error),
-                device.getName(),
-                errorMessage
-        );
-        mNotificationManager.notify(mNotificationId.incrementAndGet(), new Notification.Builder(this)
-                .setCategory(Notification.CATEGORY_ERROR)
-                .setContentTitle(getText(R.string.transfer_title))
-                .setContentText(contentText)
-                .setSmallIcon(R.drawable.ic_stat_transfer)
-                .build());
+    private void stopListening() {
+        mSocketState.compareAndSet(STATE_STARTED, STATE_STOP);
     }
 
     /**
@@ -146,79 +164,39 @@ public class TransferService extends Service {
         String[] urls = intent.getStringArrayExtra(EXTRA_URLS);
         String[] filenames = intent.getStringArrayExtra(EXTRA_FILENAMES);
 
-        // Create the bundle and persistent notification
+        // Create the bundle
         Bundle bundle = createBundle(urls, filenames);
-        final Notification.Builder builder = buildSendTransferNotification(device);
 
-        // Generate a unique ID for the transfer and show the notification
-        final int transferId = mNotificationId.incrementAndGet();
-        startForeground(transferId, builder.build());
-
-        Log.i(TAG, String.format("starting transfer %d (%d items to %s)",
-                transferId, bundle.size(), device.getName()));
-
-        final Transfer transfer = new Transfer(
-                device,
-                bundle,
-                new Transfer.Listener() {
-                    @Override
-                    public void onDeviceName(String name) {}
-
-                    @Override
-                    public void onProgress(int progress) {
-                        builder.setContentText(getText(R.string.transfer_status_transferring));
-                        builder.setProgress(100, progress, false);
-                        mNotificationManager.notify(transferId, builder.build());
-                    }
-
-                    @Override
-                    public void onSuccess() {
-                        Log.i(TAG, String.format("transfer %d succeeded", transferId));
-                        showSuccessNotification(device);
-                    }
-
-                    @Override
-                    public void onError(String message) {
-                        Log.e(TAG, String.format("transfer %d failed: %s",
-                                transferId, message));
-                        showErrorNotification(device, message);
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        if (mNumActiveTransfers.decrementAndGet() == 0) {
-                            stopForeground(false);
-                            stopSelf();
-                        }
-                    }
-                }
-        );
-
-        // Increment the number of active transfers (no way to avoid the get??)
-        mNumActiveTransfers.incrementAndGet();
-
-        // Run in a new thread
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                transfer.run();
-            }
-        };
-        thread.start();
+        // Create the transfer and transfer wrapper
+        try {
+            new TransferWrapper(this, new Transfer(device, "Android", bundle)).run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    /**
+     * Stop a transfer in progress
+     * @param intent
+     */
+    private void stopTransfer(Intent intent) {
+        TransferWrapper.stopTransfer(intent.getIntExtra(EXTRA_TRANSFER, -1));
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         switch (intent.getAction()) {
-            case ACTION_INITIATE_TRANSFER:
+            case ACTION_START_LISTENING:
+                startListening();
+                break;
+            case ACTION_STOP_LISTENING:
+                stopListening();
+                break;
+            case ACTION_START_TRANSFER:
                 startTransfer(intent);
                 break;
+            case ACTION_STOP_TRANSFER:
+                stopTransfer(intent);
         }
         return START_STICKY;
     }
