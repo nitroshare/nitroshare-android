c2e2b89dffa33fe5e801d52822a97ab0e078f0d8
Nathan Osman <nathan@quickmediasolutions.com>
2017-03-04 20:54:41 -0800

    Fix threading issues with DiscoveryAdapter by moving it into ShareActivity.

--- app/src/main/java/net/nitroshare/android/ShareActivity.java
+++ app/src/main/java/net/nitroshare/android/ShareActivity.java
@@ -1,21 +1,28 @@
 package net.nitroshare.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import net.nitroshare.android.discovery.Device;
-import net.nitroshare.android.discovery.DiscoveryAdapter;
 import net.nitroshare.android.transfer.TransferService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Display a list of devices available for receiving a transfer
@@ -27,7 +34,104 @@ public class ShareActivity extends Activity {
 
     private static final String TAG = "ShareActivity";
 
-    private DiscoveryAdapter mAdapter;
+    private NsdManager mNsdManager;
+
+    /**
+     * Adapter that discovers other devices on the network
+     */
+    class DeviceAdapter extends ArrayAdapter<String> {
+
+        /**
+         * Maintain a mapping of device IDs to discovered devices
+         */
+        private final Map<String, Device> mDevices = new HashMap<>();
+
+        /**
+         * Listener for discovery events
+         */
+        private NsdManager.DiscoveryListener mDiscoveryListener = new NsdManager.DiscoveryListener() {
+            @Override
+            public void onServiceFound(final NsdServiceInfo serviceInfo) {
+                Log.d(TAG, String.format("found \"%s\"", serviceInfo.getServiceName()));
+                if (serviceInfo.getHost() == null) {
+                    return;
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mDevices.put(serviceInfo.getServiceName(), new Device(serviceInfo));
+                        add(serviceInfo.getServiceName());
+                    }
+                });
+            }
+
+            @Override
+            public void onServiceLost(final NsdServiceInfo serviceInfo) {
+                Log.d(TAG, String.format("lost \"%s\"", serviceInfo.getServiceName()));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        remove(serviceInfo.getServiceName());
+                        mDevices.remove(serviceInfo.getServiceName());
+                    }
+                });
+            }
+
+            @Override
+            public void onDiscoveryStarted(String serviceType) {
+                Log.d(TAG, "Service discovery started");
+            }
+
+            @Override
+            public void onDiscoveryStopped(String serviceType) {
+                Log.d(TAG, "Service discovery stopped");
+            }
+
+            @Override
+            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e(TAG, "onStartDiscoveryFailed()");
+            }
+
+            @Override
+            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e(TAG, "onStopDiscoveryFailed()");
+            }
+        };
+
+        DeviceAdapter() {
+            super(ShareActivity.this, R.layout.view_simple_list_item, android.R.id.text1);
+        }
+
+        void start() {
+            mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+            mNsdManager.discoverServices(Device.SERVICE_TYPE,
+                    NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
+        }
+
+        void stop() {
+            mNsdManager.stopServiceDiscovery(mDiscoveryListener);
+        }
+
+        /**
+         * Retrieve the specified device
+         * @param position device index
+         * @return device at the specified position
+         */
+        Device getDevice(int position) {
+            return mDevices.get(getItem(position));
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+            Device device = mDevices.get(getItem(position));
+            ((TextView) convertView.findViewById(android.R.id.text1)).setText(device.getName());
+            ((TextView) convertView.findViewById(android.R.id.text2)).setText(device.getHost().getHostAddress());
+            return convertView;
+        }
+    }
+
+    private DeviceAdapter mDeviceAdapter;
 
     /**
      * Given a SEND intent, determine the absolute paths to the items to send
@@ -71,17 +175,16 @@ public class ShareActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
 
-        mAdapter = new DiscoveryAdapter(this);
-        mAdapter.start();
+        mDeviceAdapter = new DeviceAdapter();
+        mDeviceAdapter.start();
 
         final String[] filenames = resolveIntent(getIntent());
-
         final ListView listView = (ListView) findViewById(R.id.selectList);
-        listView.setAdapter(mAdapter);
+        listView.setAdapter(mDeviceAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Device device = (Device) mAdapter.getDevice(position);
+                Device device = mDeviceAdapter.getDevice(position);
 
                 Intent startTransfer = new Intent(ShareActivity.this, TransferService.class);
                 startTransfer.setAction(TransferService.ACTION_START_TRANSFER);
@@ -97,7 +200,7 @@ public class ShareActivity extends Activity {
 
     @Override
     protected void onDestroy() {
-        mAdapter.stop();
+        mDeviceAdapter.stop();
         super.onDestroy();
     }
 }
