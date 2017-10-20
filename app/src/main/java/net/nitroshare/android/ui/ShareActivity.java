1f8139070996f6ed86a752aead12010ba54661a5
Nathan Osman <nathan@quickmediasolutions.com>
2017-10-20 11:12:02 -0700

    Fix crash caused by layout issue in device adapter.

--- app/src/main/java/net/nitroshare/android/ui/ShareActivity.java
+++ app/src/main/java/net/nitroshare/android/ui/ShareActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -195,7 +196,10 @@ public class ShareActivity extends AppCompatActivity {
         @NonNull
         @Override
         public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-            convertView = super.getView(position, convertView, parent);
+            if (convertView == null) {
+                LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+                convertView = layoutInflater.inflate(R.layout.view_simple_list_item, parent, false);
+            }
             Device device = mDevices.get(getItem(position));
             ((TextView) convertView.findViewById(android.R.id.text1)).setText(device.getName());
             ((TextView) convertView.findViewById(android.R.id.text2)).setText(device.getHost().getHostAddress());
